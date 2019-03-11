21fdb0412048e91513a66200855ae6846c08ef6e
cristianccc <44088047+cristianccc@users.noreply.github.com>
2019-03-11 11:37:55 +0200

    Add project files.

---
+++ Aplicaţii client-server/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicaţii_client_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
